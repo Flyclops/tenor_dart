923c9659fa14d3133830fd897e962291d65983c0
Alex Fernandez <alex@flyclops.com>
2024-09-06 12:37:32 -0400

    update license. update server request due to docs saying not to use HttpClient() directly

--- lib/src/utilities/server_request.dart
+++ lib/src/utilities/server_request.dart
@@ -1,17 +1,15 @@
 import 'dart:convert';
-import 'dart:io';
 
+import 'package:http/http.dart' as http;
 import 'package:tenor_dart/src/constants/constants.dart';
 
 Future serverRequest(String url) async {
-  final httpClient = HttpClient();
-  final request = await httpClient.getUrl(
+  final response = await http.Client().get(
     Uri.parse(TENOR_API_URI + url),
   );
-  final response = await request.close();
+
   if (response.statusCode == 200) {
-    final json = await utf8.decoder.bind(response).join();
-    return jsonDecode(json);
+    return jsonDecode(response.body);
   } else {
     throw 'Something went wrong. Status code: ${response.statusCode}';
   }
