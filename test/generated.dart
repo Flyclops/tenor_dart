c66d211656eb53ffc6d781bb5e36669d3db3e4c5
Alex Fernandez <alex@flyclops.com>
2024-09-24 03:07:06 -0400

    remove dead code

--- test/generated.dart
+++
@@ -1,7 +0,0 @@
-// import 'package:mockito/annotations.dart';
-// import 'package:tenor_dart/tenor_dart.dart';
-
-// @GenerateNiceMocks([
-//   MockSpec<TenorResponse>(),
-// ])
-// class GeneratedMocks {}
