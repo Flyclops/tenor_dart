0b4e5233ca96556d2640fccc0b9c821c120f7ee2
Alex Fernandez <alex@flyclops.com>
2024-09-24 13:15:40 -0400

    testing coveralls

--- test/tenor_test.dart
+++ test/tenor_test.dart
@@ -21,34 +21,324 @@ void main() {
     test('Make sure it is the right type', () {
       expect(tenorClient, isA<Tenor>());
     });
+    group('.featured() >', () {
+      test('should call getGifs', () async {
+        when(
+          () => mockTenorHttpClient.getGifs(
+            any(),
+            any(),
+            any(),
+            aspectRatioRange: any(named: 'aspectRatioRange'),
+            contentFilter: any(named: 'contentFilter'),
+            limit: any(named: 'limit'),
+            mediaFilter: any(named: 'mediaFilter'),
+            sticker: any(named: 'sticker'),
+          ),
+        ).thenAnswer((_) async {
+          return TenorResponse(
+            results: [],
+          );
+        });
 
-    test('.featured()', () async {
-      when(
-        () => mockTenorHttpClient.getGifs(
-          any(),
-          any(),
-          any(),
-          aspectRatioRange: any(named: 'aspectRatioRange'),
-          contentFilter: any(named: 'contentFilter'),
-          limit: any(named: 'limit'),
-          mediaFilter: any(named: 'mediaFilter'),
-          sticker: any(named: 'sticker'),
-        ),
-      ).thenAnswer((_) async {
-        return TenorResponse(
-          aspectRatioRange: TenorAspectRatioRange.standard,
-          results: [],
-        );
+        final test = await tenorClient.featured();
+
+        verify(
+          () => mockTenorHttpClient.getGifs(
+            any(),
+            any(),
+            any(),
+            aspectRatioRange: any(named: 'aspectRatioRange'),
+            contentFilter: any(named: 'contentFilter'),
+            limit: any(named: 'limit'),
+            mediaFilter: any(named: 'mediaFilter'),
+            sticker: any(named: 'sticker'),
+          ),
+        ).called(1);
+
+        expect(test, isNotNull);
+      });
+    });
+    group('.search() >', () {
+      test('should return null if provided an empty string', () async {
+        final response = await tenorClient.search('  ');
+        expect(response, isNull);
+      });
+      test('should call getGifs', () async {
+        when(
+          () => mockTenorHttpClient.getGifs(
+            any(),
+            any(),
+            any(),
+            aspectRatioRange: any(named: 'aspectRatioRange'),
+            contentFilter: any(named: 'contentFilter'),
+            limit: any(named: 'limit'),
+            mediaFilter: any(named: 'mediaFilter'),
+            sticker: any(named: 'sticker'),
+          ),
+        ).thenAnswer((_) async {
+          return TenorResponse(results: []);
+        });
+
+        final test = await tenorClient.search('domino');
+
+        verify(
+          () => mockTenorHttpClient.getGifs(
+            any(),
+            any(),
+            any(),
+            aspectRatioRange: any(named: 'aspectRatioRange'),
+            contentFilter: any(named: 'contentFilter'),
+            limit: any(named: 'limit'),
+            mediaFilter: any(named: 'mediaFilter'),
+            sticker: any(named: 'sticker'),
+          ),
+        ).called(1);
+
+        expect(test, isNotNull);
+      });
+    });
+    group('.searchSuggestions() >', () {
+      test('should return an empty list if search is empty', () async {
+        final response = await tenorClient.searchSuggestions('  ');
+        expect(response, []);
+      });
+      test('should call request - success', () async {
+        final testSuggestions = ['domino is fun', 'domino is cool'];
+        when(
+          () => mockTenorHttpClient.request(
+            any(),
+            any(),
+          ),
+        ).thenAnswer((_) async {
+          return {
+            'results': testSuggestions,
+          };
+        });
+
+        final response = await tenorClient.searchSuggestions('domino');
+
+        verify(
+          () => mockTenorHttpClient.request(
+            any(),
+            any(),
+          ),
+        ).called(1);
+
+        expect(response, testSuggestions);
+      });
+      test('should call request - success', () async {
+        final testSuggestions = ['domino is fun', 'domino is cool'];
+        when(
+          () => mockTenorHttpClient.request(
+            any(),
+            any(),
+          ),
+        ).thenAnswer((_) async {
+          return {
+            'results': testSuggestions,
+          };
+        });
+
+        final response = await tenorClient.searchSuggestions('domino');
+
+        verify(
+          () => mockTenorHttpClient.request(
+            any(),
+            any(),
+          ),
+        ).called(1);
+
+        expect(response, testSuggestions);
+      });
+      test('should call request - results null', () async {
+        when(
+          () => mockTenorHttpClient.request(
+            any(),
+            any(),
+          ),
+        ).thenAnswer((_) async {
+          return {
+            'results': null,
+          };
+        });
+
+        final response = await tenorClient.searchSuggestions('domino');
+
+        verify(
+          () => mockTenorHttpClient.request(
+            any(),
+            any(),
+          ),
+        ).called(1);
+
+        expect(response, []);
       });
 
-      final test = await tenorClient.featured(
-        aspectRatioRange: TenorAspectRatioRange.standard,
-        limit: 5,
-        mediaFilter: [TenorMediaFormat.mediumGif],
-        sticker: true,
-      );
+      test('should call request - results empty', () async {
+        when(
+          () => mockTenorHttpClient.request(
+            any(),
+            any(),
+          ),
+        ).thenAnswer((_) async {
+          return {};
+        });
 
-      expect(test?.aspectRatioRange, TenorAspectRatioRange.standard);
+        final response = await tenorClient.searchSuggestions('domino');
+
+        verify(
+          () => mockTenorHttpClient.request(
+            any(),
+            any(),
+          ),
+        ).called(1);
+
+        expect(response, []);
+      });
+    });
+    group('.trendingSearchTerms() >', () {
+      test('should call request - success', () async {
+        final testSuggestions = ['domino is fun', 'domino is cool'];
+        when(
+          () => mockTenorHttpClient.request(
+            any(),
+            any(),
+          ),
+        ).thenAnswer((_) async {
+          return {
+            'results': testSuggestions,
+          };
+        });
+
+        final response = await tenorClient.trendingSearchTerms();
+
+        verify(
+          () => mockTenorHttpClient.request(
+            any(),
+            any(),
+          ),
+        ).called(1);
+
+        expect(response, testSuggestions);
+      });
+      test('should call request - results null', () async {
+        when(
+          () => mockTenorHttpClient.request(
+            any(),
+            any(),
+          ),
+        ).thenAnswer((_) async {
+          return {
+            'results': null,
+          };
+        });
+
+        final response = await tenorClient.trendingSearchTerms();
+
+        verify(
+          () => mockTenorHttpClient.request(
+            any(),
+            any(),
+          ),
+        ).called(1);
+
+        expect(response, []);
+      });
+      test('should call request - results empty', () async {
+        when(
+          () => mockTenorHttpClient.request(
+            any(),
+            any(),
+          ),
+        ).thenAnswer((_) async {
+          return {};
+        });
+
+        final response = await tenorClient.trendingSearchTerms();
+
+        verify(
+          () => mockTenorHttpClient.request(
+            any(),
+            any(),
+          ),
+        ).called(1);
+
+        expect(response, []);
+      });
+    });
+    group('.autocomplete() >', () {
+      test('should return an empty list if provided an empty string', () async {
+        final response = await tenorClient.autocomplete('  ');
+        expect(response, []);
+      });
+      test('should call request - success', () async {
+        final testAutocomplete = ['domino'];
+        when(
+          () => mockTenorHttpClient.request(
+            any(),
+            any(),
+          ),
+        ).thenAnswer((_) async {
+          return {
+            'results': testAutocomplete,
+          };
+        });
+
+        final response = await tenorClient.autocomplete('dom');
+
+        verify(
+          () => mockTenorHttpClient.request(
+            any(),
+            any(),
+          ),
+        ).called(1);
+
+        expect(response, testAutocomplete);
+      });
+      test('should call request - results null', () async {
+        when(
+          () => mockTenorHttpClient.request(
+            any(),
+            any(),
+          ),
+        ).thenAnswer((_) async {
+          return {
+            'results': null,
+          };
+        });
+
+        final response = await tenorClient.autocomplete('dom');
+
+        verify(
+          () => mockTenorHttpClient.request(
+            any(),
+            any(),
+          ),
+        ).called(1);
+
+        expect(response, []);
+      });
+      test('should call request - results empty', () async {
+        when(
+          () => mockTenorHttpClient.request(
+            any(),
+            any(),
+          ),
+        ).thenAnswer((_) async {
+          return {};
+        });
+
+        final response = await tenorClient.autocomplete('dom');
+
+        verify(
+          () => mockTenorHttpClient.request(
+            any(),
+            any(),
+          ),
+        ).called(1);
+
+        expect(response, []);
+      });
     });
   });
 }
